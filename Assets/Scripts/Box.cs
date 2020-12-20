d59380676f36ec2b55b136b09f38e9760640a490
Paola Guarasci <paolaguarasci@gmail.com>
2020-12-20 11:11:10 +0100

    cambio livello

--- Assets/Scripts/Box.cs
+++ Assets/Scripts/Box.cs
@@ -4,21 +4,36 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Box : MonoBehaviour {
+
+    public bool m_OnCross;
+
     public bool Move (Vector2 direction) {
         if (BoxBlocked (transform.position, direction)) {
             return false;
         } else {
             transform.Translate (direction);
-
             // Test for on cross 
-            // testForOnCross();
-
+            TestForOnCross ();
             return true;
         }
 
     }
 
+    void TestForOnCross () {
+        GameObject[] crosses = GameObject.FindGameObjectsWithTag ("Cross");
+        foreach (var cross in crosses) {
+            if (transform.position.x == cross.transform.position.x && transform.position.y == cross.transform.position.y) {
+                GetComponent<SpriteRenderer> ().color = Color.red;
+                m_OnCross = true;
+                return;
+            }
+        }
+    }
+
     bool BoxBlocked (Vector3 position, Vector2 direction) {
+
+        if (m_OnCross) return true;
+
         Vector2 newPos = new Vector2 (position.x, position.y) + direction;
         GameObject[] walls = GameObject.FindGameObjectsWithTag ("Wall");
         foreach (var wall in walls) {
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,13 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour {
     private bool m_ReadyForInput;
     public Player m_Player;
 
+    public GameObject m_NextButton;
+
     public LevelBuilder m_LevelBuilder;
 
+    private void Start () {
+        // m_NextButton.SetActive (false);
+        ResetScene ();
+    }
     void Update () {
         Vector2 moveInput = new Vector2 (Input.GetAxisRaw ("Horizontal"), Input.GetAxisRaw ("Vertical"));
         moveInput.Normalize ();
@@ -22,8 +29,45 @@ public class GameManager : MonoBehaviour {
         }
     }
 
-    private void Start () {
-        m_LevelBuilder.Build ();
-        m_Player = FindObjectOfType<Player>();
+    public void NextLevel () {
+        // m_NextButton.SetActive (false);
+        m_LevelBuilder.NextLevel ();
+        StartCoroutine (ResetSceneASync ());
+    }
+
+    public void ResetScene () {
+        StartCoroutine (ResetSceneASync ());
+    }
+
+    IEnumerator ResetSceneASync () {
+        Debug.Log ("ResetScene");
+        if (SceneManager.sceneCount > 1) {
+            AsyncOperation asyncUnload = SceneManager.UnloadSceneAsync ("Level");
+            while (!asyncUnload.isDone) {
+                yield return null;
+                Debug.Log ("Unloading...");
+            }
+            Debug.Log ("Unload ok");
+            Resources.UnloadUnusedAssets ();
+
+            AsyncOperation asyncLoad = SceneManager.LoadSceneAsync ("Level", LoadSceneMode.Additive);
+            while (!asyncLoad.isDone) {
+                yield return null;
+                Debug.Log ("Loading...");
+            }
+            Debug.Log ("Scena caricata");
+            SceneManager.SetActiveScene (SceneManager.GetSceneByName ("Level"));
+            m_LevelBuilder.Build ();
+            m_Player = FindObjectOfType<Player> ();
+            Debug.Log ("Livello caricato");
+        }
+    }
+    bool IsLevelComplete () {
+        Box[] boxes = FindObjectsOfType<Box> ();
+        foreach (var box in boxes) {
+            if (!box.m_OnCross) return false;
+        }
+
+        return true;
     }
 }
